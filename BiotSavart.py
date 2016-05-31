2c35640d150aea950a45d48f46ef28a9b9a8bcf1
piliph <p.klinkhammer@fz-juelich.de>
2016-05-31 10:47:37 +0200

    Matrixversion von BiotSavart aktualisiert

--- BiotSavart.py
+++ BiotSavart.py
@@ -49,43 +49,28 @@ def BiotSavart(x, J):
         intr += cross(J(y), (x-y)/norm(x - y)**3)
     
     return 1 / (4 * math.pi) * (intl + intr)
-            
-def oldBiotSavartMatrix(x, jl, jr):
-    n = 100
-    h = 1. / n
-    Kl = lambda x,y : cross(jl(y), (x-y)/norm(x - y)**3)
-    Kr = lambda x,y : cross(jr(y), (x-y)/norm(x - y)**3)
-    L = zeros((n/2, n), float)
-    R = zeros((n/2, n), float)
-    for j in range(n/2):
-        for i in range(1, n - 1):
-            y = array([i, j])
-            L[i][j] = 2 * Kl(x, y)
-            y = array([i + 0.5, j])
-            R[i][j] = 2 * Kr(x, y)
-        y = array([0, j])
-        L[0][j] = Kl(x, y)
-        y = array([1, j])
-        L[n-1][j] = Kl(x, y)
-        y = array([0.5, j])
-        R[0][j] = Kr(x, y)
-        y = array([i + 0.5, 1])
-        R[n-1][j] = Kr(x, y)
-            
-    return h/(8*math.pi) * (L + R)
     
-def BiotSavartMatrix(x, jl):
-    n = 100
-    m = n / 4
-    h = 1. / n
-    Kl = lambda x,y : cross(jl(y), (x-y)/norm(x - y)**3)
-    M = zeros((n, n), float)
-    for (i, y) in enumerate(itertools.product(range(1, m-1), range(2))):
-        y = array(y)
-        M[i][0] = 2 * Kl(x, y)
-    y = array([0, j])
-    M[0][0] = Kl(x, y)
-    y = array([1, j])
-    M[n-1][0] = Kl(x, y)
+def BiotSavartMatrix(J, n=100, m=36):
+    #n Auswwertungsstellen
+    #m Messstellen
+    BS = zeros((m, 2 * n), float)
+    discJ = zeros((2 * n, 1), float)
+    yList = zeros((n, 1), float)
+    xList = zeros((m, 1), float)
+    for (i, y) in enumerate(itertools.product(linspace(-1, 1, sqrt(n)/2), (0, 1, sqrt(n)/2))):
+        yList[i] = array(y)
+        
+    for (i, x) in enumerate(itertools.chain(itertools.product(linspace(-1, 1, sqrt(m)/2), range(2)), itertools.product((-1, 1), linspace(0, 1, sqrt(m)/2)))):
+        xList[i] = array(x)     
+        
+    for (i, y) in enumerate(yList):    
+        for (j, x) in enumerate(xList):
+            #Matrix aufstellen
+            BS[j][2*i] += (x[2] - y[2]) * norm(x - y)**3
+            BS[j][2*i + 1] += (x[1] - y[1]) * norm(x - y)**3
             
-    return h/(8*math.pi) * M
+        #diskrete J's
+        discJ[2*i] = J(y)[0]
+        discJ[2*i + 1] = J(y)[1]
+            
+    return BS, discJ
