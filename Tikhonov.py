1f65683100af0c2e4cc56f29a773354edbbb35e0
HennyWilly <henny-willy@hotmail.de>
2016-06-06 18:40:05 +0200

    Im l curve ist der Wurm drin

--- Tikhonov.py
+++ Tikhonov.py
@@ -19,11 +19,30 @@ def tikhonov(A, b, alpha, allowNegative):
     else:
         return spOpt.nnls(A1, np.squeeze(b1))[0]
     
-def optimal_tikhonov_parameter(A, b):
+def l_curve(A, b, n):
     """
     Hier koennte Ihr PythonDoc stehen
     """
+    tt = linspace(0,1,n)
     
-    #Das ist offensichtlich nicht immer der optiale Wert, nur ein Platzhalter...
-    return 1/10.0
+    resArray = np.zeros(n)
+    normArray = np.zeros(n)
+    alphaArray = np.zeros(n)
     
+    i = 0
+    for t in tt:
+        x, res, norm = tkhnv.tikhonov(A, b, t) 
+        if res:     
+            resArray[i] = res
+            normArray[i] = norm
+            alphaArray[i] = t
+        i = i + 1
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    
+    plt.plot(resArray, normArray, 'ro')
+    for j in range(0, n, 10):
+        ax.annotate('(%s)' % alphaArray[j], xy=(resArray[j], normArray[j]), textcoords='data')
+    plt.loglog()
+    plt.show()
